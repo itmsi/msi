8338cc0a531384d66dc88774a9a25a79e079f454
dev-harris <52947077+dev-harris@users.noreply.github.com>
2026-03-16 14:45:07 +0700

    update quot number

--- src/pages/QuotationITI/Manage/Create.tsx
+++ src/pages/QuotationITI/Manage/Create.tsx
@@ -1062,7 +1062,7 @@ export default function CreateQuotation() {
                                             <Input
                                                 id="quotation_number"
                                                 type="text"
-                                                value={`###/IEC-MSI/${monthRoman}/${new Date().getFullYear()}`}
+                                                value={`###/ITI/${monthRoman}/${new Date().getFullYear()}`}
                                                 disabled={true}
                                             />
                                         </div>
