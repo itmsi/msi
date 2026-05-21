58304cfbe2222565448e44343c4dfe1584662f4a
dev-harris <52947077+dev-harris@users.noreply.github.com>
2026-05-21 13:41:47 +0700

    update

--- src/pages/Netsuite/SalesOrders/Edit.tsx
+++ src/pages/Netsuite/SalesOrders/Edit.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { MdKeyboardArrowLeft, MdOutlineSync } from 'react-icons/md';
+import { MdOutlineSync } from 'react-icons/md';
 import { FaSave, FaCopy } from 'react-icons/fa';
 import { PermissionGate } from '@/components/common/PermissionComponents';
 import PageMeta from '@/components/common/PageMeta';
