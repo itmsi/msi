065b696b90188a54d62bc5d2d42feb533e55d72f
dev-harris <52947077+dev-harris@users.noreply.github.com>
2026-09-23 14:26:48 +0700

    update action button (remove kondisi failed)

--- src/pages/Netsuite/PurchaseOrder/Edit.tsx
+++ src/pages/Netsuite/PurchaseOrder/Edit.tsx
@@ -744,7 +744,7 @@ export default function Edit() {
                                 Cancel
                             </Button>
                             {(poDetail?.status_proccess !== 'PROCESSING') && (<>
-                                {((poDetail?.approvalstatus === 1 && poDetail.nextapprover === "") && poDetail?.status_proccess !== 'FAILED') && (
+                                {((poDetail?.approvalstatus === 1 && poDetail.nextapprover === "")) && (<>
                                     <PermissionGate permission={["create", "update"]}>
                                         <Button
                                             type="button"
@@ -756,7 +756,24 @@ export default function Edit() {
                                             Submit Approval
                                         </Button>
                                     </PermissionGate>
-                                )}
+                                    <PermissionGate permission={["create", "update"]}>
+                                        <Button
+                                            onClick={handleSubmit}
+                                            className="px-6 flex items-center gap-2 rounded-full"
+                                            disabled={isSubmitting}
+                                        >
+                                            {isSubmitting ? (<>
+                                                <svg className="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                                </svg>
+                                                Processing...
+                                            </>) : (<>
+                                                <FaSave className={`mr-2 h-4 w-4 ${isSubmitting ? 'animate-spin' : ''}`} /> Update Purchase Order
+                                            </>)}
+                                        </Button>
+                                    </PermissionGate>
+                                </>)}
                                 {(poDetail?.approvalstatus === 2 || poDetail?.po_status_label === 'Pending Receipt') && (
                                     <PermissionGate permission={["create", "update"]}>
                                         <Button
@@ -783,26 +800,6 @@ export default function Edit() {
                                         </Button>
                                     </PermissionGate>
                                 )}
-                                {(((formData.approvalstatus === 1 || formData.approvalstatus === null) && (formData.nextapprover === null || formData.nextapprover === ''))) && (
-                                    <PermissionGate permission={["create", "update"]}>
-                                        <Button
-                                            onClick={handleSubmit}
-                                            className="px-6 flex items-center gap-2 rounded-full"
-                                            disabled={isSubmitting}
-                                        >
-                                            {isSubmitting ? (<>
-                                                <svg className="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                                </svg>
-                                                Processing...
-                                            </>) : (<>
-                                                <FaSave className={`mr-2 h-4 w-4 ${isSubmitting ? 'animate-spin' : ''}`} /> Update Purchase Order
-                                            </>)}
-                                        </Button>
-                                    </PermissionGate>
-                                )}
-
                                 {((poDetail?.po_status_label === 'Pending Receipt' || poDetail?.po_status_label === 'Pending Billing/Partially Received') && poDetail?.po_id !== null && !editReceive) && (
                                     <PermissionGate permission={["create", "update"]}>
                                         <Button
